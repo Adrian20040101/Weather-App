c5a118a6a54a55c44c2fa2ef25b4d3f4af2b60a6
Adrian <adrianoinacaru5@gmail.com>
2024-04-08 14:41:06 +0300

    made a template object for displaying weather information and created a search bar that suggests results based on input

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,95 +1,19 @@
-import { Auth } from './components/authentication';
-import { auth } from './config/firebase';
 import './App.css';
-import { db } from './config/firebase';
-import { useEffect, useState } from 'react';
-import { getDocs, doc, collection, addDoc, deleteDoc, updateDoc} from 'firebase/firestore';
+import Search from './components/search/search';
+import CurrentWeather from './components/current-weather/current-weather';
 
 function App() {
 
-  const [locationList, setLocationList] = useState([]);
-  const locationsCollection = collection(db, "locations");
-
-  // new location state
-  const [newLocationCity, setNewLocationCity] = useState('');
-  const [newLocationTemperature, setNewLocationTemperature] = useState(0);
-
-  // update temperature state
-  const [updatedTemp, setUpdatedTemp] = useState(0);
-
-  const getLocationList = async () => {
-    // read the data from the database
-    // set the location list
-    try {
-      const data = await getDocs(locationsCollection);
-      const filteredData = data.docs.map((doc) => ({
-        ...doc.data(), id: doc.id}))
-      setLocationList(filteredData);
-    } catch (err) {
-      console.error(err);
-    }
-};
-
-  useEffect(() => {
-    getLocationList();
-  }, [])
-
-  const onSubmitLocation = async () => {
-    try {
-      await addDoc(locationsCollection, {
-      city: newLocationCity, 
-      temperature: newLocationTemperature,
-      userId: auth?.currentUser?.uid
-      });
-
-      getLocationList();
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const deleteLocation = async (id) => {
-    const locationDoc = doc(db, "locations", id)
-    await deleteDoc(locationDoc);
-  };
-
-  const updateLocationTemperature = async (id) => {
-    const locationDoc = doc(db, "locations", id)
-    await updateDoc(locationDoc, { temperature: updatedTemp});
-  };
+  const handleOnSearchChange = (data) => {
+    const [lat, lon] = data.value.split(" ");
+    
+    const currentWeatherFetch = fetch()
+  }
 
   return (
-    <div className="App">
-      <Auth />
-
-    <div>
-      <input
-         placeholder='City...' 
-         onChange={(e) => setNewLocationCity(e.target.value)}
-      />
-      <input 
-        placeholder='Temperature...' 
-        type='number' 
-        onChange={(e) => setNewLocationTemperature(Number(e.target.value))}
-      />
-      <button onClick={onSubmitLocation}> Submit location </button>
-    </div>
-
-      <div>
-        {locationList.map((location) => (
-          <div>
-            <h1> {location.city} </h1>
-            <p> {location.temperature} </p>
-
-            <button onClick={() => deleteLocation(location.id)}> Delete Location </button>
-            
-            <input 
-              placeholder='New Temperature' 
-              onChange={(e) => setUpdatedTemp(e.target.value)} />
-            <button onClick={() => updateLocationTemperature(location.id)}> Update Temperature </button>
-          </div>
-        ))}
-      </div>
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange}/>
+      <CurrentWeather />
     </div>
   );
 }
---
+++ weatherapp/src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'ec74fa9834msh50e383cf8b7f74dp159310jsnfe7939bd8321',
+      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+  };
+
+  export const url = "https://wft-geo-db.p.rapidapi.com/v1/geo";
